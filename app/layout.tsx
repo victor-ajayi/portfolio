78f23116355ebf7876aa1dd40f301032029152ae
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2024-02-22 00:00:16 +0300

    update styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="mx-auto max-w-[1200px] py-20">{children}</main>
+        <main className="mx-auto max-w-[800px] py-20 px-10">{children}</main>
       </body>
     </html>
   );
