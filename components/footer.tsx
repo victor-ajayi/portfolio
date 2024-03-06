e05f9d03bb87bd5002cefd661363b1184ccb5b5e
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2024-03-06 04:05:03 +0300

    fix footer padding

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,8 +8,8 @@ export default function Footer({ children }: Props) {
       <div className="flex flex-col justify-center w-full mt-4">{children}</div>
       <div className="w-full mt-10">
         <hr />
-        <div className="flex items-center">
-          <p className="mt-3 inline-block mr-auto">
+        <div className="flex pt-3">
+          <p className="inline-block mr-auto">
             Built with Next.js & TailwindCSS
           </p>
           <p className="inline-block">© 2024</p>
