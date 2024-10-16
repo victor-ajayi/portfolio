3f2a401fa423656c68b6bef25487b7bb0394d00c
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2024-10-16 18:58:51 +0300

    update bio

--- components/bio.tsx
+++ components/bio.tsx
@@ -2,15 +2,13 @@ export default function Bio() {
   return (
     <section className="card group rounded-2xl border px-5 py-3 transition-all duration-300 ease-in-out md:h-fit md:w-full">
       <p className="text-justify text-lg font-normal leading-[22px]">
-        Howdy 👋🏽, I&apos;m Victor.{" "}
+        Howdy 👋🏽, I&apos;m Victor. Nice to meet you!{" "}
         <span className="text-secondary">
-          I&apos;m a <span className="text-primary">software developer</span>{" "}
-          and I love building things. Having started as a{" "}
-          <span className="text-primary">Python</span> developer, over time
-          I&apos;ve become more comfortable with exploring my other interests;
-          I&apos;m now entering a more rounded role in{" "}
-          <span className="text-primary">fullstack</span> development with
-          React.
+          I'm a software developer and I love building things. Currently looking
+          for <span className="text-primary">backend developer role.</span> I'm
+          an explorer by nature so I have lots of interests both in tech and
+          otherwise. I'm a musician; I play the guitar, bass guitar and a little
+          bit of piano. I also love chess and sports.
         </span>
       </p>
     </section>
