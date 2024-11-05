97d20df1d970a13644df885e8240697b1db02ecf
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2024-11-05 23:53:24 +0300

    escape char

--- components/bio.tsx
+++ components/bio.tsx
@@ -4,13 +4,14 @@ export default function Bio() {
       <p className="text-justify text-lg font-normal leading-[22px]">
         Howdy 👋🏽, I&apos;m Victor. Nice to meet you!{" "}
         <span className="text-secondary">
-          I'm a Python developer and I love building things. Currently looking
-          for <span className="text-primary">backend developer</span> position.
+          I&apos;m a Python developer and I love building things. Currently
+          looking for <span className="text-primary">backend developer</span>{" "}
+          position.
           <br />
           <br />
-          I'm an explorer by nature so I have lots of interests both in tech and
-          otherwise. I'm a musician; I play the guitar, bass guitar and a little
-          bit of piano. I love chess, football and tennis.
+          I&apos;m an explorer by nature so I have lots of interests both in
+          tech and otherwise. I&apos;m a musician; I play the guitar, bass
+          guitar and a little bit of piano. I love chess, football and tennis.
         </span>
       </p>
     </section>
